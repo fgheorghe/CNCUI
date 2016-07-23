214866724c36a9535aadb5cc6e15c8e394ac303a
Grosan Flaviu Gheorghe <fgheorghe@grosan.co.uk>
2016-07-23 13:18:12 +0100

    Added constructor and mutators.

--- stl-viewer/stl-viewer.js
+++ stl-viewer/stl-viewer.js
@@ -1,1 +1,22 @@
-var STLViewer = function() {};
+var STLViewer = function(canvas, engine) {
+    this.setCanvas(canvas);
+    this.setEngine(engine);
+};
+
+STLViewer.prototype.setCanvas = function(canvas) {
+    this.canvas = canvas;
+    return this;
+};
+
+STLViewer.prototype.setEngine = function(engine) {
+    this.engine = engine;
+    return this;
+};
+
+STLViewer.prototype.getCanvas = function() {
+    return this.canvas;
+};
+
+STLViewer.prototype.getEngine = function() {
+    return this.engine;
+};
